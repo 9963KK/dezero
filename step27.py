dcc2be4443c78f25c7a77946c70d86e6496328fa
9963KK <1061893965@qq.com>
2024-08-10 13:57:51 +1000

    27

---
+++ step27.py
@@ -0,0 +1,14 @@
+# 实现高阶导数
+# 泰勒展开的导数
+import numpy as np
+from dezero import Function
+class Sin(Function):
+    def forward(self, x):
+        y = np.sin(x)
+        return y
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = gy * np.cos(x)
+        return gx
+def sin(x):
+    return Sin()(x)
